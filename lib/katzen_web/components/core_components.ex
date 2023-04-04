f93c15f7166aedba18b6639f5c2ccecbdf147b77
Alexey Sokolov <alexvyber@gmail.com>
2023-04-05 00:16:14 +0300

    comment button from core components

--- lib/katzen_web/components/core_components.ex
+++ lib/katzen_web/components/core_components.ex
@@ -202,27 +202,28 @@ defmodule KatzenWeb.CoreComponents do
       <.button>Send!</.button>
       <.button phx-click="go" class="ml-2">Send!</.button>
   """
-  attr :type, :string, default: nil
-  attr :class, :string, default: nil
-  attr :rest, :global, include: ~w(disabled form name value)
 
-  slot :inner_block, required: true
+  # attr :type, :string, default: nil
+  # attr :class, :string, default: nil
+  # attr :rest, :global, include: ~w(disabled form name value)
 
-  def button(assigns) do
-    ~H"""
-    <button
-      type={@type}
-      class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
-        @class
-      ]}
-      {@rest}
-    >
-      <%= render_slot(@inner_block) %>
-    </button>
-    """
-  end
+  # slot :inner_block, required: true
+
+  # def button(assigns) do
+  #   ~H"""
+  #   <button
+  #     type={@type}
+  #     class={[
+  #       "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
+  #       "text-sm font-semibold leading-6 text-white active:text-white/80",
+  #       @class
+  #     ]}
+  #     {@rest}
+  #   >
+  #     <%= render_slot(@inner_block) %>
+  #   </button>
+  #   """
+  # end
 
   @doc """
   Renders an input with label and error messages.
