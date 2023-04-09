54cdb2b51c90a72c5d184f3262cff9194aedc235
Alexey Sokolov <alexvyber@gmail.com>
2023-04-09 18:42:21 +0300

    comment out storybook js

--- assets/js/storybook.js
+++ assets/js/storybook.js
@@ -2,10 +2,9 @@
 // require connect parameters, uncomment the following lines and declare them as
 // such:
 
-import * as Hooks from "./hooks";
-import * as Params from "./params";
-import * as Uploaders from "./uploaders";
-
-(function () {
-  window.storybook = { Hooks, Params, Uploaders };
-})();
+// import * as Hooks from "./hooks"
+// import * as Params from "./params"
+// import * as Uploaders from "./uploaders"
+// ;(function () {
+//   window.storybook = { Hooks, Params, Uploaders }
+// })()
